38cb722972b12b64367d313996d2049a2a8ff8f8
Taran Cacacho <tarancacacho@gmail.com>
2019-07-06 15:05:36 -0700

    Add cats resource, with validations, constraints, and #age instance method

---
+++ app/models/cat.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  COLORS = %w(black tabby white blue chocolate cinnamon lilac lavender red sable brown cream).freeze
+
+  validates :birth_date, presence: true
+  validates :color, presence: true, inclusion: COLORS
+  validates :name, presence: true, uniqueness: true
+  validates :sex, presence: true, inclusion: %w(M F)
+  validates :description, presence: true
+
+  def age
+    time_ago_in_words(birth_date)
+  end 
+end
