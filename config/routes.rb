36ee6aac4f37021b046af201e490f9e4e57d3321
Taran Cacacho <tarancacacho@gmail.com>
2019-07-06 15:04:55 -0700

    Add CRUEND routes for cats resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: redirect("/cats")
+
+  resources :cats, only: [:create, :index, :show, :new, :edit, :update]
 end
