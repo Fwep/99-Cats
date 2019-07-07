30938431efdb32703f57f9eea3c2d36d3adbbde0
Taran Cacacho <tarancacacho@gmail.com>
2019-07-06 18:27:20 -0700

    Add custom routes to approve/deny requests

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root to: redirect("/cats")
 
   resources :cats, only: [:create, :index, :show, :new, :edit, :update]
-  resources :cat_rental_requests, only: [:new, :create]
+  resources :cat_rental_requests, only: [:new, :create] do
+    member do 
+      post 'approve'
+      post 'deny'
+    end
+  end
 end
